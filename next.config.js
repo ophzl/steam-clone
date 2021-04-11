280bd50d0320f51fbbcd839738fe9e58ca0a3c35
Florian LEROUX <florian.leroux3@laposte.net>
2021-04-11 19:41:40 +0200

    feat(pwa): add bare minimum for pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-    future: {
-      webpack5: true,
-    },
-  }
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  future: {
+    webpack5: true,
+  },
+  pwa: {
+    dest: "public",
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,36 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="An open-source game launcher and game store"
+        />
+        <meta
+          name="keywords"
+          content="vapor;steam;floriaaan;ophzl;game;games"
+        />
         <title>Vapor</title>
+        <link rel="manifest" href="/manifest.json" />
+        {/* <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png" /> */}
+        <meta name="theme-color" content="#ffb700" />
       </Head>
       <AnimateSharedLayout>
         <Component {...pageProps} />
